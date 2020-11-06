98cc0a753ba72f3c4d34046848ce11193100f24c
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-11-06 04:12:20 +0000

    refactor the code base

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const imageContainer = document.getElementById('image-container');
-const loader = document.getElementById('loader');
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
 
 let ready = false;
 let imagesLoaded = 0;
@@ -8,7 +8,7 @@ let photosArray = [];
 
 // Unsplash API
 const count = 5;
-const apiKey = 'YOUR_API_KEY';
+const apiKey = "jFgS8tteGD425f4oZfygQVaVnD6gt6GucN2yyz3xFek";
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
 // Check if all images were loaded using 'ready'
@@ -32,24 +32,23 @@ function setAttributes(element, attributes) {
 function displayPhotos() {
   imagesLoaded = 0;
   totalImages = photosArray.length;
-  // console.log('total images', totalImages);
   // Run function for each object in photosArray
-  photosArray.forEach(photo => {
+  photosArray.forEach((photo) => {
     // Create <a> to link to full photo
-    const item = document.createElement('a');
+    const item = document.createElement("a");
     setAttributes(item, {
       href: photo.links.html,
-      target: '_blank',
+      target: "_blank",
     });
     // Create <img> for photo
-    const img = document.createElement('img');
+    const img = document.createElement("img");
     setAttributes(img, {
       src: photo.urls.regular,
       alt: photo.alt_description,
       title: photo.alt_description,
     });
     // Event Listener, check when each is finished loading
-    img.addEventListener('load', imageLoaded);
+    img.addEventListener("load", imageLoaded);
     // Put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -68,7 +67,7 @@ async function getPhotos() {
 }
 
 // Check to see if scrolling near bottom of page, Load More Photos
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (
     window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
     ready
